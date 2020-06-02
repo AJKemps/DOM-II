9fbcf15893526a6c6b2d035c69f25e4fd3f5f622
Alex Kemper <Kemper@Alexanders-MacBook-Air.local>
2020-06-02 17:48:38 -0400

    first five event listeners added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
 // Your code goes here
+
+console.log('project is up');
+
+// Nav Buttons
+const homeButton = document.querySelector('nav:nth-of-type(1) a');
+
+// 1 Turns text blue on click of Home and stop propagation
+homeButton.addEventListener('click', function() {
+    document.body.style.color = "blue";
+    console.log('home button clicked for blue text');
+    event.stopPropagation();
+});
+
+// 2 Turns text red on click anywhere
+document.addEventListener('click', function() {
+    document.body.style.color = "red";
+    console.log('clicked anywhere for red text')
+    event.stopPropagation();
+});
+
+// 3 Text turns black on dblclick anywhere
+document.addEventListener('dblclick', function() {
+    if (document.body.style.color = "") {
+        document.body.style.color = 'black';
+    }
+    console.log('dblclick anywhere for black text')
+});
+
+// 4 Removes bus image
+function removeBus(event) {
+    if (event.key === 'r') {
+        document.querySelector('.intro  img').style.display = "none";
+        console.log('"r" button hit, bus pic removed');
+    }
+}
+
+document.addEventListener('keypress', removeBus); 
+
+// 5 Adds bus image
+function addBus(event) {
+    if (event.key === 'a') {
+        document.querySelector('.intro  img').style.display = "";
+        console.log('"a" button hit, bus pic added');
+    }
+}
+
+document.addEventListener('keypress', addBus); 
